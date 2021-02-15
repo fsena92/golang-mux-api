bacd0fa1718c736bcdc5ea1b8bf523c36e39de73
Federico Sena <federico.sena@mercadolibre.com>
2021-02-15 17:18:41 -0300

    Adding methods in post service test for mock

--- service/post-service_test.go
+++ service/post-service_test.go
@@ -1,97 +1,106 @@
 package service
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/fsena92/golang-mux-api/entity"
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/mock"
-// )
+	"github.com/fsena92/golang-mux-api/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
 
-// type MockRepository struct {
-// 	mock.Mock
-// }
+type MockRepository struct {
+	mock.Mock
+}
 
-// func (mock *MockRepository) Save(post *entity.Post) (*entity.Post, error) {
-// 	args := mock.Called()
-// 	result := args.Get(0)
-// 	return result.(*entity.Post), args.Error(1)
-// }
+func (mock *MockRepository) Save(post *entity.Post) (*entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*entity.Post), args.Error(1)
+}
 
-// func (mock *MockRepository) FindAll() ([]entity.Post, error) {
-// 	args := mock.Called()
-// 	result := args.Get(0)
-// 	return result.([]entity.Post), args.Error(1)
-// }
+func (mock *MockRepository) FindByID(id string) (*entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*entity.Post), args.Error(1)
+}
+func (mock *MockRepository) Delete(post *entity.Post) error {
+	args := mock.Called()
+	return args.Error(0)
+}
 
-// func TestFindAll(t *testing.T) {
-// 	mockRepo := new(MockRepository)
+func (mock *MockRepository) FindAll() ([]entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.([]entity.Post), args.Error(1)
+}
+func TestFindAll(t *testing.T) {
+	mockRepo := new(MockRepository)
 
-// 	var identifier int64 = 1
-// 	post := entity.Post{
-// 		ID:    identifier,
-// 		Title: "A",
-// 		Text:  "B",
-// 	}
-// 	// Setup expectations
-// 	mockRepo.On("FindAll").Return([]entity.Post{post}, nil)
+	var identifier int64 = 1
+	post := entity.Post{
+		ID:    identifier,
+		Title: "A",
+		Text:  "B",
+	}
+	// Setup expectations
+	mockRepo.On("FindAll").Return([]entity.Post{post}, nil)
 
-// 	testService := NewPostService(mockRepo)
+	testService := NewPostService(mockRepo)
 
-// 	result, _ := testService.FindAll()
+	result, _ := testService.FindAll()
 
-// 	//Mock Assertion: behavioral
-// 	mockRepo.AssertExpectations(t)
+	//Mock Assertion: behavioral
+	mockRepo.AssertExpectations(t)
 
-// 	//Data Assetion
-// 	assert.Equal(t, "A", result[0].Title)
-// 	assert.Equal(t, "B", result[0].Text)
-// 	assert.Equal(t, 1, identifier)
-// }
+	//Data Assetion
+	assert.Equal(t, "A", result[0].Title)
+	assert.Equal(t, "B", result[0].Text)
+	assert.Equal(t, 1, identifier)
+}
 
-// func TestCreate(t *testing.T) {
-// 	mockRepo := new(MockRepository)
+func TestCreate(t *testing.T) {
+	mockRepo := new(MockRepository)
 
-// 	post := entity.Post{
-// 		Title: "A",
-// 		Text:  "B",
-// 	}
+	post := entity.Post{
+		Title: "A",
+		Text:  "B",
+	}
 
-// 	//Setup expectations
-// 	mockRepo.On("Save").Return(&post, nil)
+	//Setup expectations
+	mockRepo.On("Save").Return(&post, nil)
 
-// 	testService := NewPostService(mockRepo)
+	testService := NewPostService(mockRepo)
 
-// 	result, err := testService.Create(&post)
+	result, err := testService.Create(&post)
 
-// 	//Mock Assertion: behavioral
-// 	mockRepo.AssertExpectations(t)
+	//Mock Assertion: behavioral
+	mockRepo.AssertExpectations(t)
 
-// 	//Data Assetion
-// 	assert.Equal(t, "A", result.Title)
-// 	assert.Equal(t, "B", result.Text)
-// 	assert.NotNil(t, result.ID)
-// 	assert.Nil(t, err)
-// }
+	//Data Assetion
+	assert.Equal(t, "A", result.Title)
+	assert.Equal(t, "B", result.Text)
+	assert.NotNil(t, result.ID)
+	assert.Nil(t, err)
+}
 
-// func TestValidateEmptyPost(t *testing.T) {
-// 	testService := NewPostService(nil)
+func TestValidateEmptyPost(t *testing.T) {
+	testService := NewPostService(nil)
 
-// 	err := testService.Validate(nil)
+	err := testService.Validate(nil)
 
-// 	assert.NotNil(t, err)
-// 	assert.Equal(t, "The post is empty", err.Error())
-// }
-// func TestValidateEmptyPostTitle(t *testing.T) {
-// 	post := entity.Post{
-// 		ID:    1,
-// 		Title: "",
-// 		Text:  "B",
-// 	}
-// 	testService := NewPostService(nil)
+	assert.NotNil(t, err)
+	assert.Equal(t, "The post is empty", err.Error())
+}
+func TestValidateEmptyPostTitle(t *testing.T) {
+	post := entity.Post{
+		ID:    1,
+		Title: "",
+		Text:  "B",
+	}
+	testService := NewPostService(nil)
 
-// 	err := testService.Validate(&post)
+	err := testService.Validate(&post)
 
-// 	assert.NotNil(t, err)
-// 	assert.Equal(t, "The post title is empty", err.Error())
-// }
+	assert.NotNil(t, err)
+	assert.Equal(t, "The post title is empty", err.Error())
+}
